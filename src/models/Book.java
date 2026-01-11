1900930031d7395f120d1a44943cf2570cfeeedb
YarCraSy <contact@yarcrasy.com>
2026-01-11 16:01:18 +0000

    Refactored

--- src/models/Book.java
+++ src/models/Book.java
@@ -6,4 +6,14 @@ public class Book {
     public String title;
     public String author;
     public int year;
+
+    public Book() {}
+    
+    public Book(int id, String title, String author, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
 }
